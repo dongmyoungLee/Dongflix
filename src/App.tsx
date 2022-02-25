8e942d403d69531183718d07a50c78a351c435e3
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2022-02-25 21:17:14 +0900

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path={["/", "/movies/:movieId"]}></Route>
+        <Route path={"/tv"}></Route>
+        <Route path={"/search"}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
